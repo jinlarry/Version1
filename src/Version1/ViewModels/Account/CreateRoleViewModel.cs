1c2411f302496de002ee8e108532e11a2c72b56f
larryINV <gothistyt@hotmail.com>
2016-05-20 20:27:06 +1200

    Master version of our project Basic login function and authority control

---
+++ src/Version1/ViewModels/Account/CreateRoleViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace Version1.ViewModels.Account
+{
+    public class CreateRoleViewModel
+    {
+        [Required]
+        [Display(Name = "Role Name")]
+        public string RoleName { get; set; }
+    }
+}
---
+++ src/Version1/ViewModels/Account/RegisterViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Version1.ViewModels.Account
+{
+    public class RegisterViewModel
+    {
+        #region ============== New Edited ============== 
+
+        [Required]
+        [RegularExpression(pattern: @"\w+", ErrorMessage = "Please enter alphanumeric characters for this one.")]
+        [Display(Name = "First Name")]
+        [StringLength(25, MinimumLength = 1)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [RegularExpression(pattern: @"\w+", ErrorMessage = "Please enter alphanumeric characters for this one.")]
+        [Display(Name = "Last Name")]
+        [StringLength(25, MinimumLength = 1)]
+        public string LastName { get; set; }
+
+        [Required]
+        [RegularExpression(pattern: @"\d+", ErrorMessage = "Please enter digits.")]
+        public int Age { get; set; }
+
+        [Required]
+        [RegularExpression(pattern: @"\d+", ErrorMessage = "Please enter digits.")]
+        [Display(Name = "Phone Number")]
+        public string PhoneNum { get; set; }
+
+        #endregion
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
